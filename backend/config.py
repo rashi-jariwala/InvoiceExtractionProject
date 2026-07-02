4e5bc8f13ec0140aacb98a62689290e40bfd7344
RashiJariwala <rm.pharma1120@gmail.com>
2026-07-02 13:55:49 +0530

    connection string change

--- backend/config.py
+++ backend/config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv("MONGO_URI")
+    print(os.getenv("MONGO_URI"))
     DATABASE_NAME = os.getenv("DATABASE_NAME")
+    print(os.getenv("DATABASE_NAME"))
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
+
+
